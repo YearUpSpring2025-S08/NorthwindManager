00800746a7da4f5a4f0b4a36c2f4f7b443e315c8
mattchristenson <matt@rcc.team>
2025-06-11 08:30:19 -0500

    add models, ui, and build out employee

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,12 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.UserInterface;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        UserInterface ui = new UserInterface();
+        ui.display();
+
     }
 }
