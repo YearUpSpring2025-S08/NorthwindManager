2ffc6c8f8232c69e417f005d4715575a8778d250
mattchristenson <matt@rcc.team>
2025-06-11 09:48:21 -0500

    added frank-n-console

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -4,10 +4,11 @@ import com.pluralsight.models.Employee;
 
 public class UserInterface {
 
-
+    private Employee currentEmployee;
 
     public  void display(){
         System.out.println("Welcome to the Northwind Manager!");
+        currentEmployee = loginUser();
 
     }
 
