afe31e2536021dac8177b715b5446ca69a0ccfc1
mattchristenson <matt@rcc.team>
2025-06-11 10:30:22 -0500

    main menu loop complete

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -29,6 +29,63 @@ public class UserInterface {
     }
 
     private void showHomeMenu(){
-        System.out.println("Home Menu here");
+
+
+
+        while(true){
+
+            String[] menuOptions = {
+                    "list product categories",
+                    "list all products",
+                    "list products by category",
+                    "list products by price",
+                    "list all suppliers",
+                    "list products by supplier",
+                    "exit"
+            };
+
+            int userChoice = console.promptForOption(menuOptions);
+            switch (userChoice){
+                case 1:
+                    listCategoriesAll();
+                    break;
+                case 2:
+                    listProductsAll();
+                    break;
+                case 3:
+                    listProductsByCategory();
+                    break;
+                case 4:
+                    listProductsByPrice();
+                    break;
+                case 5:
+                    listSuppliersAll();
+                    break;
+                case 6:
+                    listProductsBySupplier();
+                    break;
+                case 7:
+                    System.exit(0);
+            }
+        }
+
+    }
+
+    private void listProductsBySupplier() {
+    }
+
+    private void listSuppliersAll() {
+    }
+
+    private void listProductsByPrice() {
+    }
+
+    private void listProductsByCategory() {
+    }
+
+    private void listProductsAll() {
+    }
+
+    private void listCategoriesAll() {
     }
 }
